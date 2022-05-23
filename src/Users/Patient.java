fa55bb0e7cf195d432e749068ad2bace37e135b0
Tomas Fernandez <tomyfernandez85@gmail.com>
2022-05-23 11:39:40 -0300

    request & cancel turn

--- src/Users/Patient.java
+++ src/Users/Patient.java
@@ -1,12 +1,41 @@
 package Users;
 
+import AppTurns.Turn;
+
 public class Patient extends UserWithTurns{
 
 	
 	public Patient(String user, String password) {super(user, password);}
 	
-	public void requestTurn() {
+	/*
+	Este metodo asigna un paciente a un turno para poder ocuparlo.
+	Luego lo anade a su lista de turnos personales, para por ultimo devolverlo para actualizarlo en el objecto 'Clinic'.
+	@author Tomas Fernandez
+	@date 23/05/2022
+	@params Turn : Recibe un turno como parametro
+	*/
+	public Turn requestTurn(Turn turn) {
+		turn.asingPatient(this);
+		super.addTurn(turn);
+		return turn;
 	}
-	public void cancelTurn() {
+	
+	
+	/*
+	Este metodo cancela el turno de un paciente.
+	Lo elimina de su lista de turnos, para por ultimo devolverlo para actualizarlo en el objecto 'Clinic'.
+	@author Tomas Fernandez
+	@date 23/05/2022
+	@params Turn : Recibe un turno como parametro
+	*/
+	public Turn cancelTurn(Turn turn) {
+		turn.erasePatient();
+		prepaidsHealth.remove(turn);
+		return turn;
 	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}	
 }
